88edd1022afb4b88e3919b140e73ee56da150206
asgarovf <farhad@getwhelp.com>
2022-06-12 11:05:14 +0300

    fix: navbar items

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -44,16 +44,22 @@ const Navbar = ({
         active: pathname === PATHS.home,
       },
       {
-        name: "Swap",
+        name: "Dex Tool",
         url: PATHS.swap,
         soon: false,
         active: pathname.startsWith(PATHS.swap),
       },
       {
-        name: "Market",
+        name: "Marketplace tool",
         url: PATHS.market,
         soon: false,
         active: pathname.startsWith(PATHS.market),
+      },
+      {
+        name: "Lending tool",
+        url: "#",
+        soon: true,
+        active: pathname.startsWith("#"),
       },
     ];
   }, [pathname]);
